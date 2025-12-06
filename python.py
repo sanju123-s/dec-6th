965f711dd71d6d23ad079090d7dfc929cf4361aa
sanju123-s <1ms24mc091@msrit.edu>
2025-12-06 10:00:12 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,42 @@
-print("hhjhhhiiiiiileeeetttsssgooo")
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/sanju123-s/dec-6th'
+            }
+        }
+
+        stage('Setup Python Environment') {
+            steps {
+                echo 'Setting up Python virtual environment...'
+                sh '''
+                    python3 -m venv venv
+                    . venv/bin/activate
+                    pip install --upgrade pip
+                '''
+            }
+        }
+
+        stage('Run Python Program') {
+            steps {
+                echo 'Running Python program...'
+                sh '''
+                    . venv/bin/activate
+                    python3 first.py
+                '''
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Build and execution successful!'
+        }
+        failure {
+            echo 'Build failed'
+        }
+    }
+}
+
